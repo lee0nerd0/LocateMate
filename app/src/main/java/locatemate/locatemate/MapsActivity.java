fb6a7302ffc28ab85bdad838c444a306c03a7cca
lee0nerd0 <joshslee@gmail.com>
2016-10-20 16:08:52 -0400

    Re-doing location things

--- app/src/main/java/locatemate/locatemate/MapsActivity.java
+++ app/src/main/java/locatemate/locatemate/MapsActivity.java
@@ -2,7 +2,6 @@ package locatemate.locatemate;
 
 import android.*;
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -10,6 +9,7 @@ import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -20,7 +20,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -39,21 +43,26 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.security.Timestamp;
-import java.sql.Time;
-import java.util.HashMap;
-import java.util.UUID;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     SharedRef sharedRef;
+    // Permission things
     int permissionCheck;
     static final int MY_LOCATION_REQUEST = 1;
     // android localhost is 10.0.2.2
     private static final String GET_DATA_URL = "http://10.0.2.2:3000/get_data";
-    private static final String SAVE_DATA_URL = "http://localhost:3000/save_data";
-    private Mate me = null;
+    private static final String SAVE_DATA_URL = "http://10.0.2.2:3000/save_data";
+    // Mate things
+    private Mate me;
+    private JSONArray jsonMates;
+    // Location things
+    private LocationRequest locationRequest;
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,40 +71,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         checkPermissions();
 
+        buildGoogleApiClient();
+
         sharedRef = new SharedRef(this);
 
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.getUiSettings().setMapToolbarEnabled(false);
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
 
-        JsonTask jsonTask = new JsonTask();
-        jsonTask.execute(GET_DATA_URL);
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
-        runListener();
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(googleApiClient.isConnected()){
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(100);
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        } catch (SecurityException e){
+            e.printStackTrace();
+        }
+
+        if (lastLocation != null) {
+            updateUI();
+        }
+
+    }
+
+    private void updateUI() {
+        MarkerOptions o = new MarkerOptions();
+        LatLng pos;
+        pos = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+        o.position(pos).title("ME");
+        mMap.addMarker(o);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        buildGoogleApiClient();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateUI();
+    }
+
+    synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
     }
 
     public void checkPermissions(){
         permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST);
-        } else {
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, MY_LOCATION_REQUEST);
         }
     }
 
@@ -111,11 +167,165 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 } else {
                     // Permission denied
                     Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
+                    checkPermissions();
                 }
                 return;
             }
         }
     }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setPadding(20, 20, 20, 20);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        JsonTask jsonTask = new JsonTask();
+        jsonTask.execute(GET_DATA_URL);
+
+    }
+
+//    private void drawMates(JSONArray jsonMates) {
+//        LatLng pos;
+//        MarkerOptions o = new MarkerOptions();
+//        JSONObject  mate;
+//        int counter = 0;
+//        try {
+//            for (int i = 0; i < jsonMates.length(); i++) {
+//                mate = jsonMates.getJSONObject(i);
+//                pos = new LatLng(Double.valueOf(mate.getString("lat")),Double.valueOf(mate.getString("lon")));
+//                o.position(pos).title(mate.getString("userName"));
+//                mMap.addMarker(o);
+//                counter++;
+//                Log.d("drawMates: ", "Drawing Mate #" + String.valueOf(counter));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    private class JsonTask extends AsyncTask<String, String, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(String... args) {
+
+            try {
+                JSONObject json;
+
+                URL url = new URL(args[0]);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setConnectTimeout(7000);
+
+                try {
+                    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                    out.write(me.toJSON().getBytes("UTF-8"));
+                    out.close();
+
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    json = ConvertInputToJson(in);
+
+                    jsonMates = json.getJSONArray("data");
+
+                    return jsonMates;
+
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray json) {
+            super.onPostExecute(json);
+            LatLng pos;
+            MarkerOptions o = new MarkerOptions();
+            JSONObject  mate;
+            int counter = 0;
+            try {
+                for (int i = 0; i < json.length(); i++) {
+                    mate = json.getJSONObject(i);
+                    pos = new LatLng(Double.valueOf(mate.getString("lat")),Double.valueOf(mate.getString("lon")));
+                    o.position(pos).title(mate.getString("userName"));
+                    mMap.addMarker(o);
+                    counter++;
+                    Log.d("drawMates: ", "Drawing Mate #" + String.valueOf(counter));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static JSONObject ConvertInputToJson(InputStream in){
+        BufferedReader breader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        String result ="";
+
+        try {
+            while ((line = breader.readLine()) != null) {
+                result += line;
+            }
+
+            JSONObject json = new JSONObject(result);
+
+            in.close();
+
+            return json;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+//    public static ArrayList<Mate> ConvertToMates(JSONArray jArray){
+//        ArrayList<Mate> mates = new ArrayList<>(50);
+//        JSONObject jMate;
+//        Mate mate;
+//        try {
+//            for (int i = 0; i < jArray.length(); i++) {
+//                jMate = jArray.getJSONObject(i);
+//                mate = new Mate(jMate.getString("id"),
+//                        jMate.getString("userName"),
+//                        jMate.getString("password"),
+//                        jMate.getString("groupName"),
+//                        jMate.getInt("icon"),
+//                        jMate.getString("lat"),
+//                        jMate.getString("lon"),
+//                        jMate.getString("timestamp"));
+//                mates.add(i, mate);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return mates;
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -139,154 +349,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void runListener(){
-        MyLocationListener myLocationListener = new MyLocationListener(this);
-        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-
-        // TODO: Setting DEFAULT MATE, NEED TO MOVE SOMEWHERE ELSE
-        Long tsLong = System.currentTimeMillis()/1000;
-        String ts = tsLong.toString();
-        me = new Mate(Settings.Secure.ANDROID_ID, "#myname", "", "DefaultGroup", 0, myLocationListener.getLatlng(), ts);
-        //
-
-        try {
-
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-            if (location != null) {
-                mMap.clear();
-                LatLng you = new LatLng(location.getLatitude(), location.getLatitude());
-                mMap.addMarker(new MarkerOptions().position(you).title("You are here"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(you));
-            } else {
-                //provider, minimum time to wait, distance for update, process to start
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30, 10, myLocationListener);
-
-            }
-
-        }
-        catch (SecurityException e) {
-            // User didn't provide location
-        }
-
-        MyThread thread = new MyThread();
-        thread.start();
-    }
-
-    class MyThread extends Thread {
-        @Override
-        public void run() {
-            while(true){
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (MyLocationListener.location != null) {
-                            mMap.clear();
-                            LatLng you = new LatLng(MyLocationListener.location.getLatitude(), MyLocationListener.location.getLatitude());
-                            mMap.addMarker(new MarkerOptions().position(you).title("You are here"));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(you));
-                        }
-                    }
-                });
-                try{
-                    Thread.sleep(1000);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private class JsonTask extends AsyncTask<String, JSONArray, JSONObject> {
-
-        private ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            progressDialog = new ProgressDialog(MapsActivity.this);
-            progressDialog.show(MapsActivity.this,"JSON Task", "Working...", false, true);
-        }
-
-        @Override
-        protected JSONObject doInBackground(String... args) {
-
-            try {
-                JSONArray json;
-
-                URL url = new URL(args[0]);
-
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                urlConnection.setDoOutput(true);
-                urlConnection.setDoInput(true);
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setConnectTimeout(7000);
-
-                try {
-                    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-                    out.write(me.toJSON().getBytes("UTF-8"));
-                    out.close();
-
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    json = ConvertInputToJsonArray(in);
-
-                    publishProgress(json);
-                } finally {
-                    urlConnection.disconnect();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(JSONArray... values) {
-            try {
-                Mate mate;
-                JSONArray json = values[0];
-                Toast.makeText(MapsActivity.this, "onProgressUpdate", Toast.LENGTH_LONG).show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(JSONObject json) {
-            int success = 0;
-            String message = "";
-
-            if (progressDialog != null && progressDialog.isShowing()){
-                progressDialog.dismiss();
-            }
-        }
-    }
-
-    public static JSONArray ConvertInputToJsonArray(InputStream in){
-        BufferedReader breader = new BufferedReader(new InputStreamReader(in));
-        String line;
-        String result = "";
-
-        try {
-            while ((line = breader.readLine()) != null){
-                result += line;
-            }
-
-            JSONArray json = new JSONArray(result);
-
-            in.close();
-
-            return json;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-
-    }
-
 }
--- app/src/main/java/locatemate/locatemate/Mate.java
+++ app/src/main/java/locatemate/locatemate/Mate.java
@@ -15,18 +15,22 @@ public class Mate {
     private String userName;
     private String password;
     private String groupName;
-    private Integer iconid;
-    private LatLng latLng;
+    private Integer icon;
+    private String lat;
+    private String lon;
     private String timestamp;
+    private LatLng loc;
 
-    Mate (String id, String uN, String pw, String gN, Integer i, LatLng loc, String ts) {
+    Mate (String id, String uN, String pw, String gN, Integer i, String lat, String lon, String ts) {
         setId(id);
         setUserName(uN);
         setPassword(pw);
         setGroupName(gN);
-        setIconid(i);
-        setLatLng(loc);
+        setIcon(i);
+        setLat(lat);
+        setLon(lon);
         setTimestamp(ts);
+        setLatLng();
     }
 
     // getters and setters
@@ -63,20 +67,36 @@ public class Mate {
         this.groupName = groupName;
     }
 
-    public Integer getIconid() {
-        return iconid;
+    public Integer getIcon() {
+        return icon;
     }
 
-    public void setIconid(Integer iconid) {
-        this.iconid = iconid;
+    public void setIcon(Integer icon) {
+        this.icon = icon;
     }
 
     public LatLng getLatLng() {
-        return latLng;
+        return loc;
     }
 
-    public void setLatLng(LatLng latLng) {
-        this.latLng = latLng;
+    public void setLatLng() {
+        this.loc = new LatLng(Double.valueOf(this.lat), Double.valueOf(this.lon));
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
     }
 
     public String getTimestamp() {
@@ -96,9 +116,9 @@ public class Mate {
             jsonObject.put("userName", getUserName());
             jsonObject.put("password", getPassword());
             jsonObject.put("groupName", getGroupName());
-            jsonObject.put("icon", getIconid());
-            jsonObject.put("lat", getLatLng().latitude);
-            jsonObject.put("lon", getLatLng().longitude);
+            jsonObject.put("icon", getIcon());
+            jsonObject.put("lat", getLat());
+            jsonObject.put("lon", getLon());
             jsonObject.put("timestamp", getTimestamp());
 
             return jsonObject.toString();
